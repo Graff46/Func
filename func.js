9b0db3364ad531288ba3a0733de5859a3ebcf1e7
Graff46 <macter46@yandex.ru>
2020-08-09 23:09:42 +0300

    fix

--- func.js
+++ func.js
@@ -114,113 +114,6 @@ class Func {
 }
 
 class __DOMElement {
-<<<<<<< Updated upstream
-    constructor(selector, ...funcs) {
-        if (selector instanceof Node) 
-            this.elms = [selector];
-        else if (selector instanceof NodeList)
-            this.elms = Array.from(selector);
-        else
-            this.elms = Array.from(document.querySelectorAll(selector));
-
-        for (const func of funcs) 
-            this.elms.forEach((...params) => func(...params));
-    }
-
-    splice (...arg) {
-        this.elms.splice(...arg);
-        return this;
-    }
-
-    bindStor = toMap();
-
-    bind (obj, handler) {
-        let lastKey = [];
-        let lastObj = obj;
-        const setWkey = subj => { 
-            return new Proxy(subj, {
-                get: (tar, prop, ...args) => {
-                    lastKey = prop;
-                    if (tar[prop] instanceof Object) {
-                        lastObj = lastObj[prop]; 
-                        return setWkey(Reflect.get(tar, prop, ...args));
-                    }
-                    return Reflect.get(tar, prop, ...args);
-            }});
-        }
-
-        this.__subscribleProp(obj, 'set, del');
-        
-        const sobj = setWkey(obj); 
-        let idx = 0
-        for (const node of this.elms) {
-            lastObj = obj;
-            handler(this.__meta(node), sobj, node, idx);
-
-            let [currentObj, currentKey] = [lastObj, lastKey];
-            let storI = idx;
-            if (!this.bindStor.has(currentObj))
-                this.bindStor.set(currentObj, toMap());
-            this.bindStor.get(currentObj).getArr(currentKey).push(() => handler(this.__meta(node), obj, node, storI));
-            node.addEventListener('input', eve => currentObj[currentKey] = eve.target.value);
-            idx++;
-        }
-    }
-
-    __outInStor = toMap();
-
-    __funcAddNodes (referenceNode, newNode, wrapHandler, obj, key, count, adder, nodeHandler) {
-        referenceNode.before(newNode);
-        let currNode = newNode;
-        if (nodeHandler)
-            currNode = nodeHandler(currNode);
-        wrapHandler(currNode, count, obj[key], key);
-
-        count++;
-        if (!currNode.isConnected) return;
-
-        this.elms.push(currNode);
-
-        adder[0].counter = count;
-        return this.__outInStor.get(obj).getArr(key).push([currNode, count]);
-    };
-
-    outIn (obj, handler, nodeHandler) {
-        const wrapHandler = (node, counter, val, key) => handler(this.__meta(node), val, key, node, counter);
-        const __adder = [];
-        this.__outInStor.set(obj, toMap({__wrapHandler: wrapHandler, __adder}));
-
-        const elems = Array.from(this.elms);
-        this.elms = [];
-        let working = false;
-        for (let node of elems) {
-            let counter = 0;
-            __adder.unshift({primary: node, counter: 0});
-            for (const key in obj) { 
-                working = true;
-                this.__funcAddNodes(node, node.cloneNode(true), wrapHandler, obj, key, counter, __adder, nodeHandler);
-                this.__outInStor.get(obj).set('__wrapHandler', wrapHandler).set('__adder', __adder);
-                counter++;
-            }
-            const referenceNode = document.createElement('meta');
-            referenceNode.setAttribute('func', '');
-            node.after(referenceNode);
-            node.remove();
-            __adder[0].referenceNode = referenceNode;
-        }
-        if (working)
-            this.__subscribleProp(obj, 'set, del');
-        return this;
-    }
-
-    __meta(node) { 
-        return (...funcs) => funcs.callAll(node);
-    }
-
-    __subscribleProp (obj, events) { 
-        return events.split(',').forEach(event => obj[Func.__subscribleVarName].get(event.trim()).add(this));
-    }
-=======
 	constructor(selector, ...funcs) {
 		if (selector instanceof Node) 
 			this.elms = [selector];
@@ -338,43 +231,12 @@ class __DOMElement {
 	__subscribleProp (obj, events) { 
 		return events.split(',').forEach(event => obj[Func.__subscribleVarName]?.get(event.trim()).add(this));
 	}
->>>>>>> Stashed changes
 	
 	__deleteNode (node) {
 		node.remove();
 		this.elms = this.elms.filter(elm => elm != node);
 	}
 
-<<<<<<< Updated upstream
-    __proxy_set(primaryObj, primaryProp, obj, prop, newValue) {
-        let outInStor = this.__outInStor.get(primaryObj);
-        if (outInStor) {
-            if (outInStor.get(primaryProp)) {
-                let nodeHandler;
-                if (newValue == null)
-                    nodeHandler = params => {
-						this.__deleteNode(params[0]);
-                        outInStor.delete(primaryProp);
-                    };
-                else
-                    nodeHandler = params => outInStor.get('__wrapHandler')(...params, primaryObj[primaryProp], primaryProp);
-                outInStor.get(primaryProp).forEach(params => nodeHandler(params));
-            }
-            else { 
-                const adder = outInStor.get('__adder');
-                for (const {primary, counter, referenceNode} of adder) 
-                    this.__funcAddNodes(referenceNode, primary.cloneNode(true), outInStor.get('__wrapHandler'), primaryObj, primaryProp, counter + 1, adder);
-            }
-        }
-
-        this.bindStor.get(obj)?.get(prop)?.callAll();
-    }
-
-    __proxy_del(primaryProp, obj, primaryObj) {
-        let outInStor = this.__outInStor.get(primaryObj);  
-        let paramsArr;
-        if ((outInStor) && (paramsArr = outInStor.get(primaryProp))) {
-=======
 	__proxy_set(primaryObj, primaryProp, obj, prop, newValue) {
 		this.proxyCalls = true;
 		let outInStor = this.__outInStor.get(primaryObj);
@@ -408,7 +270,6 @@ class __DOMElement {
 		let outInStor = this.__outInStor.get(primaryObj);  
 		let paramsArr;
 		if ((outInStor) && (paramsArr = outInStor.get(primaryProp))) {
->>>>>>> Stashed changes
 			for (const params of paramsArr) {
 				this.__deleteNode(params[0]);
 				outInStor.delete(primaryProp);
--- main.js
+++ main.js
@@ -7,17 +7,9 @@ let obj = {class: 8, ll: 55};
 
 const app = new Func({mytest: obj});
 
-<<<<<<< Updated upstream
-el('ol>li').outIn(app.mytest, noop)
-//     (exe, v, k, node, i) => { 
-//         content(k)(node.findAll('p', 0));
-//         content(v)(node.findAll('p', 1));
-// });
-=======
 var r = el('ol>li').repeat(app.mytest, (exe, v, k, node, i) => {
 	exe(content(k));
 	//el(node.find('p')).repeat(v, (exe2, k)=>exe2(content(k)));
 })
->>>>>>> Stashed changes
 
 //el('.txt').bind(app.mytest, (exe, data, node, i) => (i%2)? exe(value(data.io.h)): exe(value(data.gg.h)));
